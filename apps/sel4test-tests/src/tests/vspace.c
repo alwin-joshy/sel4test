dd95779b7aea9b1daae27db64cd6700c96b6ca49
Alwin Joshy <joshyalwin@gmail.com>
2023-08-16 12:28:18 +1000

    Changing to use new remap impl
    Signed-off-by: Alwin Joshy <joshyalwin@gmail.com>

--- apps/sel4test-tests/src/tests/vspace.c
+++ apps/sel4test-tests/src/tests/vspace.c
@@ -197,48 +197,11 @@ static int test_range_unmap_small(env_t env) {
     }
 
     for (int i = 0; i < NPAGE/32; i += 1) {
-        seL4_CapSet set;
-        set.c0 = frames[32*i];
-        set.c1 = frames[32*i + 1];
-        set.c2 = frames[32*i + 2];
-        set.c3 = frames[32*i + 3];
-        set.c4 = frames[32*i + 4];
-        set.c5 = frames[32*i + 5];
-        set.c6 = frames[32*i + 6];
-        set.c7 = frames[32*i + 7];
-        set.c8 = frames[32*i + 8];
-        set.c9 = frames[32*i + 9];
-        set.c10 = frames[32*i + 10];
-        set.c11 = frames[32*i + 11];
-        set.c12 = frames[32*i + 12];
-        set.c13 = frames[32*i + 13];
-        set.c14 = frames[32*i + 14];
-        set.c15 = frames[32*i + 15];
-        set.c16 = frames[32*i + 16];
-        set.c17 = frames[32*i + 17];
-        set.c18 = frames[32*i + 18];
-        set.c19 = frames[32*i + 19];
-        set.c20 = frames[32*i + 20];
-        set.c21 = frames[32*i + 21];
-        set.c22 = frames[32*i + 22];
-        set.c23 = frames[32*i + 23];
-        set.c24 = frames[32*i + 24];
-        set.c25 = frames[32*i + 25];
-        set.c26 = frames[32*i + 26];
-        set.c27 = frames[32*i + 27];
-        set.c28 = frames[32*i + 28];
-        set.c29 = frames[32*i + 29];
-        set.c30 = frames[32*i + 30];
-        set.c31 = frames[32*i + 31];
-
-        // for (int j = 0; j < 32; i++) {
-        //     set_caps[i] = frames[32*i + j];
-        // }
-        set.num = 32;
-        printf("hello world\n");
-
-
-        seL4_ARM_VSpace_Remap_Range_t remap_ret = seL4_ARM_VSpace_Remap_Range(vspace, &set, seL4_NoRights);
+        seL4_Word cap_set[32];
+        for (int j = 0; j < 32; j++) {
+            cap_set[j] = frames[32*i + j];
+        }
+        seL4_ARM_VSpace_Remap_Range_t remap_ret = seL4_ARM_VSpace_Remap_Range(vspace, cap_set, 32, seL4_NoRights);
         test_error_eq(remap_ret.error, 0);
         test_assert(remap_ret.remapped = 0xffff); 
     }
@@ -261,69 +224,64 @@ static int test_range_unmap_small(env_t env) {
 }
 DEFINE_TEST(VSPACE0011, "Test range based unmap function with small pages", test_range_unmap_small, true)
 
-// static int test_range_unmap_large(env_t env) {
-//     seL4_Word map_addr = 0x10000000;
-//     seL4_CPtr frames[NPAGE_LARGE];
-//     int error;
+static int test_range_unmap_large(env_t env) {
+    seL4_Word map_addr = 0x10000000;
+    seL4_CPtr frames[NPAGE_LARGE];
+    int error;
 
-//     seL4_CPtr pgd = vka_alloc_object_leaky(&env->vka, seL4_ARM_PageGlobalDirectoryObject, 0);
-//     seL4_CPtr pud = vka_alloc_object_leaky(&env->vka, seL4_ARM_PageUpperDirectoryObject, 0);
-//     seL4_CPtr pd = vka_alloc_object_leaky(&env->vka, seL4_ARM_PageDirectoryObject, 0);
+    seL4_CPtr pgd = vka_alloc_object_leaky(&env->vka, seL4_ARM_PageGlobalDirectoryObject, 0);
+    seL4_CPtr pud = vka_alloc_object_leaky(&env->vka, seL4_ARM_PageUpperDirectoryObject, 0);
+    seL4_CPtr pd = vka_alloc_object_leaky(&env->vka, seL4_ARM_PageDirectoryObject, 0);
 
-//     for (int i = 0; i < NPAGE_LARGE; i++) {
-//         frames[i] = vka_alloc_object_leaky(&env->vka, seL4_ARM_LargePageObject, 0);
-//         test_assert(frames[i] != 0);
-//     }
+    for (int i = 0; i < NPAGE_LARGE; i++) {
+        frames[i] = vka_alloc_object_leaky(&env->vka, seL4_ARM_LargePageObject, 0);
+        test_assert(frames[i] != 0);
+    }
 
-//     test_assert((seL4_PGDBits == 0) || pgd != 0);
-//     test_assert(pud != 0);
-//     test_assert(pd != 0);
+    test_assert((seL4_PGDBits == 0) || pgd != 0);
+    test_assert(pud != 0);
+    test_assert(pd != 0);
 
-//     seL4_CPtr vspace = (seL4_PGDBits == 0) ? pud : pgd;
-//     seL4_ARM_ASIDPool_Assign(env->asid_pool, vspace);
-// #if seL4_PGDBits > 0
-//     /* map pud into page global directory */
-//     error = seL4_ARM_PageUpperDirectory_Map(pud, vspace, map_addr, seL4_ARM_Default_VMAttributes);
-//     test_error_eq(error, seL4_NoError);
-// #endif
+    seL4_CPtr vspace = (seL4_PGDBits == 0) ? pud : pgd;
+    seL4_ARM_ASIDPool_Assign(env->asid_pool, vspace);
+#if seL4_PGDBits > 0
+    /* map pud into page global directory */
+    error = seL4_ARM_PageUpperDirectory_Map(pud, vspace, map_addr, seL4_ARM_Default_VMAttributes);
+    test_error_eq(error, seL4_NoError);
+#endif
 
-//     /* map pd into page upper directory */
-//     error = seL4_ARM_PageDirectory_Map(pd, vspace, map_addr, seL4_ARM_Default_VMAttributes);
-//     test_error_eq(error, seL4_NoError);
+    /* map pd into page upper directory */
+    error = seL4_ARM_PageDirectory_Map(pd, vspace, map_addr, seL4_ARM_Default_VMAttributes);
+    test_error_eq(error, seL4_NoError);
 
-//     for (int i = 0; i < NPAGE_LARGE; i++) {
-//         error = seL4_ARM_Page_Map(frames[i], vspace, map_addr + i * (1 << seL4_LargePageBits), seL4_NoRights, seL4_ARM_Default_VMAttributes);
-//         test_error_eq(error, 0);
-//     }
+    for (int i = 0; i < NPAGE_LARGE; i++) {
+        error = seL4_ARM_Page_Map(frames[i], vspace, map_addr + i * (1 << seL4_LargePageBits), seL4_NoRights, seL4_ARM_Default_VMAttributes);
+        test_error_eq(error, 0);
+    }
 
-//     seL4_CPtr pt = vka_alloc_object_leaky(&env->vka, seL4_ARM_PageTableObject, 0);
-//     test_assert(pt != 0);
+    seL4_CPtr pt = vka_alloc_object_leaky(&env->vka, seL4_ARM_PageTableObject, 0);
+    test_assert(pt != 0);
 
-//     /* Because a large page is already mapped at this level, we will not be able to map a page table*/
-//     error = seL4_ARM_PageTable_Map(pt, vspace, map_addr, seL4_ARM_Default_VMAttributes);
-//     test_error_eq(error, seL4_DeleteFirst);
+    /* Because a large page is already mapped at this level, we will not be able to map a page table*/
+    error = seL4_ARM_PageTable_Map(pt, vspace, map_addr, seL4_ARM_Default_VMAttributes);
+    test_error_eq(error, seL4_DeleteFirst);
 
+    seL4_Word curr_addr = map_addr;
+    seL4_Word end = map_addr + NPAGE_LARGE * (1 << seL4_LargePageBits);
 
-//     /* Because a large page is already mapped at this level, we will not be able to map a page table*/
-//     error = seL4_ARM_PageTable_Map(pt, vspace, map_addr, seL4_ARM_Default_VMAttributes);
-//     test_error_eq(error, seL4_DeleteFirst);
+    while (curr_addr < end) {
+        seL4_ARM_VSpace_Remap_Range_t unmap_ret = seL4_ARM_VSpace_Remap_Range(vspace, &frames[0], 32, seL4_AllRights);
+        test_error_eq(unmap_ret.error, 0);
+        test_assert(unmap_ret.remapped == 32);
+    }
 
-//     seL4_Word curr_addr = map_addr;
-//     seL4_Word end = map_addr + NPAGE_LARGE * (1 << seL4_LargePageBits);
+    /* Since we unmapped we should be able to do a page table map now */
+    error = seL4_ARM_PageTable_Map(pt, vspace, map_addr, seL4_ARM_Default_VMAttributes);
+    test_error_eq(error, 0);
 
-//     while (curr_addr < end) {
-//         seL4_ARM_VSpace_Remap_Range_t unmap_ret = seL4_ARM_VSpace_Remap_Range(vspace, frames[0], 32, seL4_AllRights);
-//         test_error_eq(unmap_ret.error, 0);
-//         test_assert(unmap_ret.num_remapped == 32);
-//     }
-
-//     /* Since we unmapped we should be able to do a page table map now */
-//     error = seL4_ARM_PageTable_Map(pt, vspace, map_addr, seL4_ARM_Default_VMAttributes);
-//     test_error_eq(error, 0);
-
-//     return sel4test_get_result();
-// }
-// DEFINE_TEST(VSPACE0012, "Test range based unmap function with large pages", test_range_unmap_large, true)
+    return sel4test_get_result();
+}
+DEFINE_TEST(VSPACE0012, "Test range based unmap function with large pages", test_range_unmap_large, true)
 
 // static int test_range_unmap_small_large(env_t env) {
 //     seL4_Word map_addr = 0x10000000;
